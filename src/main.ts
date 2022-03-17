1f553a3f8a014bc1b8e779306e3d55247ad5e2bc
Jessica Johann <jessorlisa@gmail.com>
2022-03-17 09:07:09 +0100

    chore: sync changes with real life project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
+import { NativeScriptNgZone, platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
+
+import { ImageCacheIt } from '@triniwiz/nativescript-image-cache-it';
 
 import { AppModule } from '@src/app/app.module';
 
 runNativeScriptAngularApp({
-    appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule, {
-        ngZoneEventCoalescing: true
+    appModuleBootstrap: (reason: string) => platformNativeScript().bootstrapModule(AppModule, {
+        ngZone: new NativeScriptNgZone()
     })
 });
+
+ImageCacheIt.enableAutoMM();
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,19 @@ module.exports = (env) => {
     // Learn how to customize:
     // https://docs.nativescript.org/webpack
 
+    webpack.chainWebpack((config) => {
+        // t&e to solve `Module not found: Error: Can't resolve 'fs' in './node_modules/source-map/lib'`
+        // thanks to @herefishyfishyfish at discord
+        // @see https://discord.com/channels/603595811204366337/890118789876834344/890178556443906078
+
+        const fallback = config.resolve.get("fallback");
+        config.resolve.set(
+            "fallback",
+            webpack.Utils.merge(fallback || {}, {
+                fs: require.resolve("@nativescript/core/"),
+            })
+        );
+    });
+
     return webpack.resolveConfig();
 };
